6d90403f476419b96eaa60468e3d3445dd8836b0
eftekhar-hossain <eftekhar.hossain@cuet.ac.bd>
2024-11-09 20:11:05 -0500

    Changed Dataset.sh

--- Datasets/download_dataset.sh
+++ Datasets/download_dataset.sh
@@ -5,20 +5,20 @@ unzip myfile.zip
 # delete the zip file
 rm myfile.zip
 
-# Download the MUTE Dataset
-gdown 'https://docs.google.com/uc?export=download&id=1ozTFUM7q27g7uckhPWUiQFwhROCiEUAc' -O file.zip
-unzip file.zip
-# delete the zip file
-rm file.zip
-
-# Download the BanglaAbuseMeme Dataset
-gdown 'https://docs.google.com/uc?export=download&id=1N-OX8xsOZt3zqFOP7veszgvoju0C7piv' -O file.zip
-unzip file.zip
-# delete the zip file
-rm file.zip
-
-# Download the BanglaAbuseMeme Dataset
+# Download the MIMOSA Dataset
 gdown 'https://docs.google.com/uc?export=download&id=1NT2koLEIVT8oOHwchY35p_Y2qCNuOqP5' -O file.zip
 unzip file.zip
 # delete the zip file
 rm file.zip
+
+## Download the MUTE Dataset
+#gdown 'https://docs.google.com/uc?export=download&id=1ozTFUM7q27g7uckhPWUiQFwhROCiEUAc' -O file.zip
+# unzip file.zip
+## delete the zip file
+# rm file.zip
+
+## Download the BanglaAbuseMeme Dataset
+#gdown 'https://docs.google.com/uc?export=download&id=1N-OX8xsOZt3zqFOP7veszgvoju0C7piv' -O file.zip
+#unzip file.zip
+## delete the zip file
+#rm file.zip
